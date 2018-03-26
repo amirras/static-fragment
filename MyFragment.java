3edcfa80c6b47bda5c99233e016029f9b7efcab7
amirras <amir_rastegar_153@yahoo.com>
2018-03-26 13:17:23 +0100

    Add files via upload

---
+++ MyFragment.java
@@ -0,0 +1,26 @@
+package com.example.android.myapplication_staticfragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ASUS on 3/25/2018.
+ */
+
+public class MyFragment extends Fragment {
+
+    public MyFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.my_fragment,container,false);
+        return rootView;
+    }
+}
